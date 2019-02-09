9046320926ce2da77208d44a164bf6277b1d80b8
HishamGad99 <44401838+HishamGad99@users.noreply.github.com>
2019-02-09 23:01:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const client = new Discord.Client();
 var prefix = "#"
 
 
+client.on('message', async msg => {
+    if(msg.content.startsWith(prefix + 'bc')) {
+      let roleW = msg.mentions.roles.first();
+      let args2 = msg.content.split(" ").slice(2).join(" ");
+       if(!msg.guild.members.get(msg.author.id).hasPermission('ADMINISTRATOR')) return msg.channel.send('Required Administrator Permission') 
+       let role = msg.guild.roles.find(`name`, roleW.name);
+       if(!role) return msg.reply(`Could't find \`${roleW.name}\` role.`).then( msgs => msgs.delete(3000)); 
+       let nomsg = 0;
+         msg.channel.send(`**- [ :mailbox_closed:  :: ${nomsg} ] ・عدد الرسائل المرسلة**`).then(msgs => {
+         role.members.forEach(m =>{
+        m.send(args2.replace('[user]', m).replace('[server]', m.guild.name).replace('[sender]', msg.author.username)).then( () =>{
+          nomsg++;
+                  if(!msgs) return;
+                  msgs.edit(`**- [ :mailbox_closed:  :: ${nomsg} ] ・عدد الرسائل المرسلة**`);
+        }).catch(e => {
+          nomsg++;
+                  if(!msgs) return;
+          msgs.edit(`**- [ :mailbox_closed:  :: ${nomsg} ] ・عدد الرسائل المرسلة**`);
+        });
+        }); 
+      });
+      }});
 
 //bc
 
